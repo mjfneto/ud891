5b767a655569cc9ef44feddce5c05b7ff7d8a0d8
Mario Freire <huege.design@gmail.com>
2019-03-02 14:07:10 -0300

    feat: adds ARIA design-specs for radiogroup and radio buttons

--- lesson5-semantics-aria/06-radio-group/radiogroup.js
+++ lesson5-semantics-aria/06-radio-group/radiogroup.js
@@ -24,6 +24,7 @@
     this.el.addEventListener('click', this.handleClick.bind(this));
 
     // Any more initialization to do here?
+    this.el.setAttribute('role', 'radiogroup');
 
     var firstButton = true;
     for (var button of this.buttons) {
@@ -35,6 +36,7 @@
       }
 
       // What about here?
+      button.setAttribute('role', 'radio');
     }
 
   }
@@ -92,6 +94,7 @@
     // Set the old button to tabindex -1
     this.focusedButton.tabIndex = -1;
     this.focusedButton.removeAttribute('checked');
+    this.focusedButton.setAttribute('aria-checked', 'false');
 
     // Set the new button to tabindex 0 and focus it
     this.focusedButton = this.buttons[this.focusedIdx];
@@ -100,6 +103,7 @@
     this.focusedButton.setAttribute('checked', '');
 
     // ... we probably want to do some stuff here, too ...
+    this.focusedButton.setAttribute('aria-checked', 'true');
 
   };
 
